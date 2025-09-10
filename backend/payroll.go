2004b6f7e531c593b87470baf3535269cd966fe8
Ronald Palay <govph.project@gmail.com>
2025-09-10 13:15:33 +0800

    style: gofmt payroll files

--- backend/payroll.go
+++ backend/payroll.go
@@ -2,5 +2,5 @@ package main
 
 // CalculateNet returns the net pay computed from gross, deductions and taxes.
 func CalculateNet(gross, deductions, taxes float64) float64 {
-    return gross - deductions - taxes
+	return gross - deductions - taxes
 }
